86171435b1f01d7299753c232051e41b4263a0ab
izzyisozaki <izzy_isozaki@hotmail.com>
2025-11-28 13:06:31 +0100

    fixed paths

--- meschini-site/main/urls.py
+++ meschini-site/main/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("contatti", views.contatti),
-    path("chisiamo", views.areaAziende,
-    path("offertelavoro", views.offerte),
+    path("area-aziende", views.area_aziende),
+    path("area-candidati", views.area_candidati)
+    path("offerte-di-lavoro", views.offerte_di_lavoro),
 
 ]
