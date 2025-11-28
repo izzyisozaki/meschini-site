8bc783b507da642bf21d14ec8e465e7a2dbd826f
izzyisozaki <izzy_isozaki@hotmail.com>
2025-11-28 11:11:45 +0100

    cont.

--- meschini-site/main/views.py
+++ meschini-site/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from .models import *
 
-
-def home(request):
-    studenti = Studenti.objects.all()
-    return render(request, 'main/index.html', {"studenti":studenti})
+# def home(request):
+#     studenti = Studenti.objects.all()
+#     return render(request, 'main/index.html', {"studenti":studenti})
 
 def contatti(request):
     return render(request, 'main/contatti.html')
